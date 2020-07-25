c9a8fe4bc17f347e27b402a67a81595bf905264e
Ruben Steen <rubensteen123@gmail.com>
2020-07-25 11:35:12 +0200

    Created confirmed middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\AbortIfBanned::class,
+            \App\Http\Middleware\CheckIfConfirmed::class,
         ],
 
         'api' => [
---
+++ app/Http/Middleware/CheckIfConfirmed.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class CheckIfConfirmed
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::check() && Auth::user()->isConfirmed() === false) {
+
+            Auth::logout();
+
+            return redirect('/')->with('warning', [
+                'title' => "Account confirmation",
+                'message' => "Your account has not yet been confirmed. Please contact @cruorzy#1337 on discord...",
+            ]);
+        }
+
+        return $next($request);
+    }
+}
