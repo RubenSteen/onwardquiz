e4e0ea0bb4ecc5ba521e5fc66af840888ead8531
Ruben Steen <rubensteen123@gmail.com>
2020-07-26 12:04:47 +0200

    Added SensioLabs Security Checker

--- run_test.sh
+++ run_test.sh
@@ -5,8 +5,11 @@ printf "\nRunning PHPUnit\n\n"
 vendor/bin/phpunit
 
 
-printf "\nRunning PHP_CodeSniffer\n\n"
+printf "\n\n\n\n\n\n\n\nRunning PHP_CodeSniffer\n\n"
 vendor/bin/phpcs
 
-printf "\nRunning PHP Copy/Paste Detector - 15 lines detection\n\n"
+printf "\n\n\n\n\n\n\n\nRunning PHP Copy/Paste Detector - 15 lines detection\n\n"
 vendor/bin/phpcpd app --min-lines=15
+
+printf "\n\n\n\n\n\n\n\nRunning SensioLabs Security Checker\n\n"
+vendor/bin/security-checker security:check composer.lock
