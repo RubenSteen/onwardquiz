0c2e011245ae3c49a8e55db5d8f51a2511141413
Ruben Steen <rubensteen123@gmail.com>
2020-07-28 09:25:28 +0000

    Default edits

--- public/js/app.js
+++ public/js/app.js
@@ -697,8 +697,8 @@ eval("// removed by extract-text-webpack-plugin//# sourceURL=[module]\n//# sourc
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Cruorzy\Documents\Repositorys\onwardquiz-test\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Cruorzy\Documents\Repositorys\onwardquiz-test\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /app/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /app/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
