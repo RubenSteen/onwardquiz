1d6009a41595f7b1e3ffc1e980965fee32c3e986
Ruben Steen <RubenSteen@users.noreply.github.com>
2021-02-02 21:08:17 +0000

    Apply fixes from StyleCI

--- resources/js/Pages/Shared/Layout.vue
+++ resources/js/Pages/Shared/Layout.vue
@@ -460,13 +460,9 @@
                                     </svg>
                                 </div>
                                 <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                    <h3 class="text-lg leading-6 font-medium text-gray-900">
-                                        Logout
-                                    </h3>
+                                    <h3 class="text-lg leading-6 font-medium text-gray-900">Logout</h3>
                                     <div class="mt-2">
-                                        <p class="text-sm leading-5 text-gray-500">
-                                            Are you sure you want to logout?
-                                        </p>
+                                        <p class="text-sm leading-5 text-gray-500">Are you sure you want to logout?</p>
                                     </div>
                                 </div>
                             </div>
